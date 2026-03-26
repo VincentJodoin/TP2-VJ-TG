73cada46ecdc6833a93bae70fdcf2401191821b7
TomGoidin <2412355@cegeplimoilou.ca>
2026-03-26 16:20:33 -0400

    Finit de changer les tests et la Librairie afin que le code compile

--- src/livres/Serie.java
+++ src/livres/Serie.java
@@ -7,6 +7,9 @@ public class Serie {
     int nom;
     ArrayList<Ouvrage> listeOuvrages = new ArrayList<>();
 
-
-
+    public Serie(ArrayList<Ouvrage> listeOuvrages, int nom)
+    {
+        this.listeOuvrages = listeOuvrages;
+        this.nom = nom;
+    }
 }
--- src/tests/TestOuvrage.java
+++ src/tests/TestOuvrage.java
@@ -88,6 +88,7 @@ public class TestOuvrage {
 
         // Test créer une série
 
+
     }
 
 
